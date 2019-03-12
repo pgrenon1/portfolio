fc4e52d98188c682ea981cb8b7352f4fb989261a
Phil <pgrenon.pg@gmail.com>
2019-03-12 11:32:13 -0400

    new bg

--- assets/js/bg.js
+++ assets/js/bg.js
@@ -1,36 +1,43 @@
-var triangles
-var state
-const states = [0, 1]
 var columnCount
 var heights = []
 var noiseIndex = 0
 var backgroundCol
-var prevMouseX
-var prevMouseY
 var lines = []
-var delta
-var fps
-
+var animationHeight = 2000
+var moveUp = false
+var cnv
 p5.disableFriendlyErrors = true
 
-function setup () {
-  columnWidth = windowWidth / 215
-  var cnv = createCanvas(windowWidth, windowHeight / 2)
+function setup() {
+  columnWidth = windowWidth / 50
+  cnv = createCanvas(windowWidth, windowHeight)
   cnv.parent('background')
-  frameRate(60)
-  backgroundCol = color(255, 255, 255)
+
+  backgroundCol = color(10, 10, 10)
   columnCount = windowWidth / columnWidth
   for (let i = 0; i < columnCount; i++) {
     lines[i] = []
   }
-  stroke(0)
+
+  // menuItems = selectAll('.menu-item')
+  // for (i in menuItems) {
+  //   menuItems[i].mousePressed(MoveUp)
+  // }
+
+  // nameButton = select('.name')
+  // nameButton.mousePressed(MoveDown)
 }
 
-function draw () {
+function draw() {
+  // if (moveUp && animationHeight > -200) {
+  //   animationHeight -= 15
+  // } else if (!moveUp && animationHeight < 2000) {
+  //   animationHeight += 15
+  // }
+
   background(backgroundCol)
-  GetNewHeights(0.005)
-  delta = mouseY / 100
-  prevMouseX = mouseX
+  GetNewHeights(0.003)
+
   for (let i = 0; i < columnCount; i++) {
     var height = GetHeight(i)
 
@@ -38,44 +45,60 @@ function draw () {
     RemoveLine(i, height)
 
     for (let j = 0; j < lines[i].length; j++) {
-      var x1 = lines[i][j][0] + delta
-      var y1 = lines[i][j][1]
-      var x2 = lines[i][j][2] - delta
-      var y2 = lines[i][j][3]
-      line(x1 - mouseX / 70, y1, x2 - mouseX / 70, y2)
+      var x1 = lines[i][j][0]
+      var y1 = lines[i][j][1] / 2
+      var x2 = lines[i][j][2]
+      var y2 = lines[i][j][3] / 2
+      if (abs(mouseX - i * windowWidth / 50) <= 100) {
+        strokeWeight(10)
+      } else {
+        strokeWeight(3)
+      }
+      stroke(255, 255, 255, 30)
+      line(x1, y1, x2, y2)
     }
   }
-  // text(Math.round(frameRate()), 0, 10)
+  textSize(15)
+  fill(255)
+  text(Math.round(frameRate()), windowWidth - 20, windowHeight - 20)
 }
 
-function GetHeight (i) {
+function GetHeight(i) {
   return heights[i]
 }
 
-function AddLine (columnIndex, height) {
+function AddLine(columnIndex, height) {
   var newLine = [
-    windowWidth / columnCount * columnIndex,
+    (windowWidth / columnCount) * columnIndex,
     height,
-    windowWidth / columnCount * columnIndex + windowWidth / columnCount,
+    (windowWidth / columnCount) * columnIndex + windowWidth / columnCount,
     height
   ]
   lines[columnIndex].push(newLine)
 }
 
-function RemoveLine (columnIndex) {
+function RemoveLine(columnIndex) {
   if (lines[columnIndex].length > 20) {
     lines[columnIndex].shift()
   }
 }
 
-function GetNewHeights (ni) {
+function GetNewHeights(ni) {
   heights = []
   noiseIndex += ni
   for (let i = 0; i < columnCount; i++) {
-    heights.push(noise(i + noiseIndex) * 1000)
+    heights.push(noise(i + noiseIndex) * animationHeight)
   }
 }
 
-function windowResized () {
-  resizeCanvas(windowWidth, windowHeight / 2)
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
+
+function MoveUp() {
+  moveUp = true
+}
+
+function MoveDown() {
+  moveUp = false
 }
